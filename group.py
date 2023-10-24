7575e479349d8fb9d615107003411e53ade762ee
Bee <99090290+beenishsanjrani@users.noreply.github.com>
2023-10-24 18:21:24 +0100

    Update group.py

--- group.py
+++ group.py
@@ -2,4 +2,40 @@
 
 # Your code to go here...
 
-my_group =
+my_group = {
+    'Jill' : {
+        'Relations': {
+            'Zalika' : 'friend',
+            'John' : 'partner'
+        },
+        'Age' : '26',
+        'Job' : 'Biologist'
+    },
+    'Zalika' : {
+        'Relations': {
+            'Jill' : 'friend'
+        },
+        'Age' : '28',
+        'Job' : 'Artist'
+    },
+    'John' : {
+        'Relations': {
+            'Jill' : 'partner'
+        },
+        'Age' : '27',
+        'Job' : 'Writer'
+    },
+    'Nash' : {
+        'Relations': {
+            'Zalika' : 'Landlord',
+            'John' : 'Cousin'
+        },
+        'Age' : '34',
+        'Job' : 'Chef'
+    }
+}
+
+Jill = group.get('Jill')
+Zalika = group.get('Zalika')
+John = group.get('John')
+Nash = group.get('Nash')
